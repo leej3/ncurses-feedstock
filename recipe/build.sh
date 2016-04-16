100ba97bf98f62e4ac2f23068eb0f09e1e55f271
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-16 12:48:21 -0400

    recipe: Build with and without widec support.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,21 @@
 
 mkdir $PREFIX/lib
 
-sh ./configure \
-	--prefix=$PREFIX \
-	--without-debug \
-	--without-ada \
-	--without-manpages \
-	--with-shared \
-	--disable-overwrite \
-	--enable-symlinks \
-	--enable-termcap \
-	--with-termlib \
-	--enable-widec \
-	--with-terminfo-dirs=/usr/share/terminfo
 
-make
-make install
+for USE_WIDEC in "" "--enable-widec"
+do
+    sh ./configure \
+	    --prefix=$PREFIX \
+	    --without-debug \
+	    --without-ada \
+	    --without-manpages \
+	    --with-shared \
+	    --disable-overwrite \
+	    --enable-symlinks \
+	    --enable-termcap \
+	    --with-termlib \
+	    $USE_WIDEC \
+	    --with-terminfo-dirs=/usr/share/terminfo
+    make
+    make install
+done
