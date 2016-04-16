d06f7f9b023174db1d3a6cb551a37efecf75e193
Jonathan Helmus <jjhelmus@gmail.com>
2016-04-16 07:48:20 -0500

    Do not place header files in subdirectory
    Use the --includedir flag in configure to specify that the headers should be
    installed in $PREFIX/include not the default $PREFIX/include/ncursesw/
    
    closes #3

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ mkdir $PREFIX/lib
 sh ./configure --prefix=$PREFIX \
     --without-debug --without-ada --without-manpages \
     --with-shared --disable-overwrite --enable-termcap \
-    --with-termlib \
+    --with-termlib --includedir=$PREFIX/include \
     --enable-widec --with-terminfo-dirs=/usr/share/terminfo
 
 make -j$(getconf _NPROCESSORS_ONLN)
