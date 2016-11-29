468ef987e833489790ac82a8afc5fa1e71054b35
Jonathan Helmus <jjhelmus@gmail.com>
2016-11-29 10:15:31 -0600

    Build long prefix package using conda-build 2.xi
    Use defaults channel as provider of long prefix packages in build environment

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -41,7 +41,15 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
+# install conda-build 2.x to build a long prefix
+conda install --yes --quiet conda-build=2
+conda info
+
 # Embarking on 1 case(s).
-    conda build /recipe_root --quiet || exit 1
+    # use the default channel as a provider of long prefix packages
+    conda build -c defaults /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+# inspect the prefix lengths of the built packages
+conda inspect prefix-lengths /feedstock_root/build_artefacts/linux-64/*.tar.bz2
 EOF
